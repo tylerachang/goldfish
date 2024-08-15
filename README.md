a2ac8943bf129066ae669d88ff5b2698eabab9ec
tylerachang <tylerachang@gmail.com>
2024-08-15 16:38:16 -0700

    Add Colab demo.

--- README.md
+++ README.md
@@ -4,8 +4,11 @@
 The models are trained on 5MB, 10MB, 100MB, and 1GB of text in each language when available, after accounting for the fact that some languages require more UTF-8 bytes than others to encode comparable text.
 When 1GB of text is not available for a language, we also release a "full" model trained on our entire dataset for that language.
 The Goldfish reach lower perplexities than state-of-the-art multilingual models for many low-resource languages ([Chang et al., 2024](link)), and they can be used as baselines, fine-tuning sources, or augmentations to larger models for low-resource NLP research.
+Google Colab demo [here](https://colab.research.google.com/drive/1rHFpnQsyXJ32ONwCosWZ7frjOYjbGCXG?usp=sharing).
 
+<picture>
 <img src="goldfish_map.png" alt="Goldfish map." width="90%" />
+</picture>
 
 For training and evaluations details, see our paper, [Goldfish: Monolingual Language Models for 350 Languages (Chang et al., 2024)](link).
 In the current repository, we include the original training and evaluation code, along with dataset and evaluation info as tsv files.
@@ -13,7 +16,7 @@ The available Goldfish languages are listed in `constants.py`.
 
 To use the Goldfish models, we recommend using the models available on Hugging Face: https://huggingface.co/goldfish-models
 
-We provide sample code in `example_generate_text.py` and `example_score_text.py`, or in a browser through [this](link) Google Colab.
+We provide sample code in `example_generate_text.py` and `example_score_text.py`, or in a browser through [this](https://colab.research.google.com/drive/1rHFpnQsyXJ32ONwCosWZ7frjOYjbGCXG?usp=sharing) Google Colab.
 Each Goldfish model is 125M parameters at the largest, which can easily be run on a free Google Colab GPU or even on CPU.
 
 ## Citation.
--- example_score_text.py
+++ example_score_text.py
@@ -25,19 +25,12 @@ HF_CACHE = 'hf_cache'
 MAX_SEQ_LEN = 512
 
 
-def main(args):
-    # Load config, tokenizer, and model.
-    goldfish_model = 'goldfish-models/' + args.model
-    config = AutoConfig.from_pretrained(goldfish_model, cache_dir=HF_CACHE)
-    tokenizer = AutoTokenizer.from_pretrained(goldfish_model, cache_dir=HF_CACHE)
-    model = AutoModelForCausalLM.from_pretrained(
-            goldfish_model, config=config, cache_dir=HF_CACHE)
+# Return the log-probability of the target text given the input text.
+def score_text(model, tokenizer, input_text, target_text):
     loss = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id, reduction='none')
-    # Load onto GPU.
-    if torch.cuda.is_available(): model = model.cuda()
     # Prepare inputs.
-    input_tokens = tokenizer([args.input_text], add_special_tokens=False)['input_ids'][0]
-    target_tokens = tokenizer([args.target_text], add_special_tokens=False)['input_ids'][0]
+    input_tokens = tokenizer([input_text], add_special_tokens=False)['input_ids'][0]
+    target_tokens = tokenizer([target_text], add_special_tokens=False)['input_ids'][0]
     sequence_tokens = input_tokens + target_tokens
     sequence_tokens.insert(0, tokenizer.cls_token_id)  # Start token.
     assert len(sequence_tokens) <= MAX_SEQ_LEN
@@ -67,7 +60,20 @@ def main(args):
     logprobs = -1.0 * losses
     # Log-probability of entire target text is the sum of token log-probs.
     summed_logprobs = torch.sum(logprobs, dim=-1).item()
-    print(summed_logprobs)
+    return summed_logprobs
+
+
+def main(args):
+    # Load config, tokenizer, and model.
+    goldfish_model = 'goldfish-models/' + args.model
+    config = AutoConfig.from_pretrained(goldfish_model, cache_dir=HF_CACHE)
+    tokenizer = AutoTokenizer.from_pretrained(goldfish_model, cache_dir=HF_CACHE)
+    model = AutoModelForCausalLM.from_pretrained(
+            goldfish_model, config=config, cache_dir=HF_CACHE)
+    # Load onto GPU.
+    if torch.cuda.is_available(): model = model.cuda()
+    logprob = score_text(model, tokenizer, args.input_text, args.target_text)
+    print(logprob)
 
 
 if __name__ == "__main__":
